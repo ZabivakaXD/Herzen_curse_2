7a37f0ee58974f768071dfa2d2ed753f4becb167
ZabivakaXD <kostyab2@mail.ru>
2024-10-08 01:21:37 +0300

    lab_1

--- prog/Calculate.py
+++ prog/Calculate.py
@@ -1,20 +1,44 @@
-def calculate(a, b, opperand):
-    if opperand == '+':
+def calculate(a, b, operand):
+    """
+    Функция для выполнения арифметических операций.
+
+    Функция получает два числа и арифметическую операцию.
+    Определяет какая это из операций: '+', '-', '*', '/'
+
+    Резульатом является число
+    """
+    if operand == '+':
         return a + b
-    elif opperand == '-':
+    elif operand == '-':
         return a - b
-    elif opperand == '*':
+    elif operand == '*':
         return a * b
-    elif opperand == '/':
-        return a / b
+    elif operand == '/':
+        if b != 0:
+            return a / b
+        else: 
+            return 'Нельзя делить на ноль'
     else:
-        return 'unknown operation'
+        return 'Неизвестная операция'
+
+def main():
+    """
+    Главная функция для запуска калькулятора.
+    
+    Запрашивает у пользователя ввод двух чисел и операции.
+    
+    Выводит результат вычисления.
+    """
+    a, operand, b = map(str, input("Введите выражение ").split())
+    print("Результат выражения ", calculate(int(a), int(b), operand))
+    
+#main()
 
 def test_calculate():
     assert calculate(1, 2, '+') == 3, 'dont work'
-def test_calculate1():
     assert calculate('1','2','+') == 3, 'dont work'
-def test_calculate2():
-    assert calculate(1, 2, '+') == 3, 'dont work'
+    assert calculate(5, 0, '/') == 'Нельзя делить на ноль', 'dont work'
+    assert calculate(1, 2, '*') == 2, 'dont work'
+    assert calculate(1, 2, '-') == -1, 'dont work'
 
-test_calculate2()
+test_calculate()
--- prog/GuessTheNumber.py
+++ prog/GuessTheNumber.py
@@ -1,18 +1,52 @@
 def binSearch(massiv, findNumber):
-  global comparison
-  left = -1
-  right = len(massiv)
-  while left < right - 1:
-      mid = (left + right) // 2
-      comparison += 1
-      if massiv[mid] < findNumber:
-          left = mid
-      else:
-          right = mid
-  return massiv[right]
+    """
+    Поиск числа с помощью бинарного поиска
 
-findNumber = int(input("Загадайте число "))
-start, end = map(int, input("Напишите начало и конец диапазона поиска ").split())
-massive = list(range(start, end + 1))
-comparison = 0
-print("Загаданное число " + str(binSearch(massive, findNumber)) + " Нашли с " + str(comparison) + " попытки")
+    Функция берёт число по середине и сравнивает его с числом, которое нужно найти.
+    Если число из серединны меньше нужного числа оно смещает левую границу на место среднего числа,
+    если число из середины больше нужного числа оно смещает правую границу на место среднего числа.
+
+    Функция возвращает загаданное число и попытку, с которой оно было найденно
+    """
+    comparison = 0
+    left = -1
+    right = len(massiv)
+    while left < right - 1:
+        mid = (left + right) // 2
+        comparison += 1
+        if massiv[mid] < findNumber:
+            left = mid
+        else:
+            right = mid
+    return massiv[right], comparison
+
+def GuestNumber(findNumber, start, end):
+    """
+    Основная функция запуска
+
+    Функция принимает число, которое нужно найти, а также начало и конец диапазона поиска.
+    Создаёт массив из этого диапазона и вызывает функцию поиска, откуда и приходит ответ
+
+    Функция возвращает загаданное число и попытку, с которой оно было найденно
+    """
+    #findNumber, start, end = map(int, input("Загадайте число, а также начало и конец диапазона поиска ").split())
+    massiv = list(range(start, end + 1))
+    answer = binSearch(massiv, findNumber)
+    #print("Загаданное число ", answer[0], " Нашли с ", answer[1], " попытки")
+    return answer
+
+#GuestNumber()
+
+def test_GuessNumber():
+    answer = GuestNumber(1, 1, 10)
+    assert answer[0] == 1 and answer[1] == 3, "dont work"
+    answer = GuestNumber(3, 1, 10)
+    assert answer[0] == 3 and answer[1] == 3, "dont work"
+    answer = GuestNumber(79, 1, 100)
+    assert answer[0] == 79 and answer[1] == 6, "dont work"
+    answer = GuestNumber(79, -100, 100)
+    assert answer[0] == 79 and answer[1] == 7, "dont work"
+    answer = GuestNumber(893, 1, 1000)
+    assert answer[0] == 893 and answer[1] == 10, "dont work"
+
+test_GuessNumber()
