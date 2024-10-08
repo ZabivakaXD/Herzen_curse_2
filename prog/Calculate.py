cdd2e8c510984d8d4556b3aab2516ab774ff28f1
ZabivakaXD <kostyab2@mail.ru>
2024-10-08 14:46:25 +0300

    lab_2_rework

--- prog/Calculate.py
+++ prog/Calculate.py
@@ -1,3 +1,26 @@
+import logging
+
+logging.basicConfig(filename='prog\Calculate.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def log_decorator(func):
+    """Decorate
+    """
+    def wrapper(a, b, operand):
+        """Closure
+
+        Логируется информация о том какие операнды и какая арифметическая операция собираются поступить на вход функции.
+        Затем внутри того же closure следует сам вызов функции calculate(...).
+        А затем, после этого снова логирование, но уже с результатом выполнения вычисления, проделанного в этой функции.
+
+        Возвращает результат выражения
+        """
+        logging.info(f'Вызов функции {func.__name__} с параметрами: a={a}, b={b}, operand="{operand}"')
+        result = func(a, b, operand)
+        logging.info(f'Результат выполнения функции {func.__name__}: {result}')
+        return result
+    return wrapper
+
+@log_decorator
 def calculate(a, b, operand):
     """
     Функция для выполнения арифметических операций.
@@ -5,7 +28,7 @@ def calculate(a, b, operand):
     Функция получает два числа и арифметическую операцию.
     Определяет какая это из операций: '+', '-', '*', '/'
 
-    Резульатом является число
+    Результат является число
     """
     if operand == '+':
         return a + b
