5710d634e83e6734267790bda50d959ce16d3a64
ZabivakaXD <kostyab2@mail.ru>
2024-11-05 16:36:07 +0300

    lab4

--- prog/weather.py
+++ prog/weather.py
@@ -6,9 +6,10 @@ with urllib.request.urlopen(url) as request:
     html = request.read()
 
 bs = BeautifulSoup(html, "html.parser")
-print(bs)
 
-print(bs.find("span", {"class":"ef118"}).string)
+#print(bs.text)
+print("Температура", bs.find("span", {"class":"ef050"}).text, ", ощущается как", bs.find("span", {"class":"ef051"}).text, chr(127777))
+print("Скорость ветра", bs.find("span", {"class":"ef118"}).text, chr(127744))
 
 # вывести 3 строки температура, скорость ветра, видимость и какие-нибудь эмодзи
 # для 2.3 sabplots данные погоды
