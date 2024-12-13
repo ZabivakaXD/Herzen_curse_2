ff65f6e89b317e402105f097c16159d91459fd04
ZabivakaXD <kostyab2@mail.ru>
2024-12-13 23:16:50 +0300

    commit

--- prog/web-prog_json-posts.py
+++ prog/web-prog_json-posts.py
@@ -20,5 +20,11 @@ def main():
     # Отправка результата методом POST
     post_response = requests.post('https://docs.google.com/forms/u/0/d/e/1FAIpQLSeZyDJ_68Mj7io5vtjyNqul7ceNE1t5Z5KkkN7foqxbIcUsbg/formResponse', data={'entry.364005965': result})
 
+    # Проверка успешности POST-запроса
+    if post_response.status_code == 200:
+        print("Данные успешно отправлены!")
+    else:
+        print("Ошибка при отправке данных:", post_response.status_code)
+        
 main()
 
